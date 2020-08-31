939e2850a3cfedf3f142f61522b7146abec36d70
bbs232 <38854535+bbs232@users.noreply.github.com>
2020-08-31 13:58:19 +0300

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,20 @@
+function mycity(){
+    alert("yes you got it");
+}
+
+function pushButton(buttonValue)
+{
+     if (buttonValue == 'C')
+     {
+          document.getElementById('screen').value = '';
+     }
+     else
+     {
+          document.getElementById('screen').value += buttonValue;
+     }
+}
+function calculate(equation)
+{
+     var answer = eval(equation);
+     document.getElementById('screen').value = answer;
+}
